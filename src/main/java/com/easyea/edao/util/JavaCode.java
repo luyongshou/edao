6c2fb1609779dd373129af06ff248bf2e4a9b68b
louis <louis@easyea.com>
2013-09-04 10:09:46 +0800

    添加分区管理的常用功能以及JavaCode的对象来定义一个生成Java代码时对象方便以后动态编译

---
+++ src/main/java/com/easyea/edao/util/JavaCode.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.easyea.edao.util;
+
+/**
+ *
+ * @author louis
+ */
+public class JavaCode {
+    private String packName;
+    private String className;
+    private String code;
+
+    /**
+     * @return the packName
+     */
+    public String getPackName() {
+        return packName;
+    }
+
+    /**
+     * @param packName the packName to set
+     */
+    public void setPackName(String packName) {
+        this.packName = packName;
+    }
+
+    /**
+     * @return the className
+     */
+    public String getClassName() {
+        return className;
+    }
+
+    /**
+     * @param className the className to set
+     */
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    /**
+     * @return the code
+     */
+    public String getCode() {
+        return code;
+    }
+
+    /**
+     * @param code the code to set
+     */
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
---
+++ src/main/java/com/easyea/edao/util/PartitionUtil.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.easyea.edao.util;
+
+import com.easyea.edao.ddls.PostgresqlDdlManager;
+import com.easyea.edao.partition.NumberRange;
+import com.easyea.edao.partition.PartitionParam;
+import com.easyea.edao.partition.TimeInterval;
+import com.easyea.edao.partition.TimeRange;
+import com.easyea.edaodemo.entity.Demo;
+import com.easyea.internal.CodeBuilder;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ *
+ * @author louis
+ */
+public class PartitionUtil {
+    public static JavaCode getPartitionManager(Class entity, PartitionParam param) {
+        JavaCode java = new JavaCode();
+        String packName  = getPartManagerPackage(entity);
+        String className = getPartManagerName(entity);
+        java.setPackName(packName);
+        java.setClassName(className);
+        
+        CodeBuilder code = new CodeBuilder();
+        code.a("package ").a(packName).a(";").r(2);
+        if (param instanceof NumberRange) {
+            NumberRange nrp = (NumberRange)param;
+            code.a("import com.easyea.edao.PartitionManager;").r(1);
+            code.a("import com.easyea.edao.partition.NumberInterval;").r(1);
+            code.a("import com.easyea.edao.partition.NumberRange;").r(1);
+            code.a("import com.easyea.edao.partition.PartitionParam;").r(1);
+            code.a("import ").a(entity.getName()).a(";").r(2);
+            
+            code.a("public class ").a(className).a(" implements PartitionManager {").r(1);
+            code.t(1).a("public <T> String getExtTableString(T entity, PartitionParam param) {").r(1);
+            code.t(2).a("String s = \"\";").r(1);
+            code.t(2).a("if (param == null) {").r(1);
+            code.t(3).a("return s;").r(1);
+            code.t(2).a("}").r(1);
+            code.t(2).a(entity.getSimpleName()).a(" demo = (").a(entity.getSimpleName()).a(")entity;").r(1);
+            code.t(2).a("NumberRange nrp = (NumberRange)param;").r(1);
+            code.t(2).a("if (nrp.getInterval() == NumberInterval.NUMBER_MILLION) {").r(1);
+            code.t(3).a("s = \"__N\" + nrp.getCount() + \"M_\" + demo.get")
+                    .a(nrp.getField().substring(0, 1).toUpperCase(Locale.ENGLISH))
+                    .a(nrp.getField().substring(1)).a("()").a("/(nrp.getCount()*1000000);").r(1);
+            code.t(2).a("}").r(1);
+            code.t(2).a("return s;").r(1);
+            code.t(1).a("}").r(1);
+            code.a("}");
+        } else if (param instanceof TimeRange) {
+            TimeRange trp = (TimeRange)param;
+            code.a("import com.easyea.edao.PartitionManager;").r(1);
+            code.a("import com.easyea.edao.partition.TimeInterval;").r(1);
+            code.a("import com.easyea.edao.partition.TimeRange;").r(1);
+            code.a("import com.easyea.edao.partition.PartitionParam;").r(1);
+            code.a("import ").a(entity.getName()).a(";").r(1);
+            code.a("import java.text.SimpleDateFormat;").r(2);
+            
+            code.a("public class ").a(className).a(" implements PartitionManager {").r(1);
+            code.t(1).a("public <T> String getExtTableString(T entity, PartitionParam param) {").r(1);
+            code.t(2).a("String s = \"\";").r(1);
+            code.t(2).a("if (param == null) {").r(1);
+            code.t(3).a("return s;").r(1);
+            code.t(2).a("}").r(1);
+            code.t(2).a(entity.getSimpleName()).a(" demo = (").a(entity.getSimpleName()).a(")entity;").r(1);
+            code.t(2).a("TimeRange trp = (TimeRange)param;").r(1);
+            code.t(2).a("SimpleDateFormat tbFormat = null;").r(1);
+            code.t(2).a("if (trp.getInterval() == TimeInterval.TIME_DAY) {").r(1);
+            code.t(3).a("tbFormat = new SimpleDateFormat(\"yyyyMMdd\");").r(1);
+            code.t(2).a("} else if (trp.getInterval() == TimeInterval.TIME_MONTH) {").r(1);
+            code.t(3).a("tbFormat = new SimpleDateFormat(\"yyyyMM\");").r(1);
+            code.t(2).a("} else if (trp.getInterval() == TimeInterval.TIME_YEAR) {").r(1);
+            code.t(3).a("tbFormat = new SimpleDateFormat(\"yyyy\");").r(1);
+            code.t(2).a("}").r(1);
+            code.t(2).a("if (tbFormat != null) {").r(1);
+            code.t(3).a("s = \"__T\" + trp.getCount() + \"_\" +  ")
+                    .a("tbFormat.format(demo.get")
+                    .a(trp.getField().substring(0, 1).toUpperCase(Locale.ENGLISH))
+                    .a(trp.getField().substring(1)).a("());").r(1);
+            code.t(2).a("}").r(1);
+            code.t(2).a("return s;").r(1);
+            code.t(1).a("}").r(1);
+            code.a("}");
+        }
+        java.setCode(code.toString());
+        return java;
+    }
+    
+    public static String getPartManagerPackage(Class entity) {
+        String cname = entity.getName();
+        int    index = cname.indexOf(".entity.");
+        if (index == -1) {
+            index = 0;
+        }
+        String pack = cname.substring(0, index) + ".partitionm";
+        return pack;
+    }
+    
+    public static String getPartManagerName(Class entity) {
+        String s = entity.getSimpleName() + "Partm";
+        return s;
+    }
+    
+    public static void main(String[] args) {
+        PartitionParam trp = null;
+        PostgresqlDdlManager ddlm = new PostgresqlDdlManager(Demo.class);
+        try {
+            trp = ddlm.getPartitionParam();
+        } catch (Exception e) {
+            
+        }
+        JavaCode java = PartitionUtil.getPartitionManager(Demo.class, trp);
+        System.out.println(java.getPackName() + "." + java.getClassName());
+        System.out.println(java.getCode());
+    }
+}
