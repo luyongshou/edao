02a306fa9d5833ca46bd06470962fbf5bd2fc03d
louis <louis@easyea.com>
2015-05-20 10:50:47 +0800

    添加二进制json格式数据类型注释

---
+++ src/main/java/com/easyea/edao/annotation/Jsonb.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.easyea.edao.annotation;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
+
+/**
+ * 二进制的JSON类型
+ * @author louis
+ */
+@Target({METHOD, FIELD})
+@Retention(RUNTIME)
+public @interface Jsonb {
+    
+}
